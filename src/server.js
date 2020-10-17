c3bf72e2bd820cdd1454699ae22757b0fa567fce
Pedro Henrique Fernandes Lobo <lobophf@gmail.com>
2020-10-16 22:23:57 -0300

    feat: Config the npm hbs
    To setup the server engine.

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('lalalalala brizola')
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
+const server = express();
+console.log(pages.orphanages)
+server
+	.use(express.static('public'))
+	.set('views', path.join(__dirname, "views"))
+	.set('view engine', 'hbs')
+	.get('/', pages.index)
+	.get('orphanage', pages.orphanage)
+	.get('orphanages', pages.orphanages)
+	.get('create-orphanage', pages.createOrphanage)
+
+server.listen(5500)
